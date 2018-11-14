7c04a3822100a0fd0f613b922414813d327abcb5
Himenon <k.himeno314@gmail.com>
2018-11-15 03:26:54 +0900

    ci(deploy): 自動デプロイ用のスクリプトを追加する

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-echo "Deploy tasks"
+: "Before script of Deploy" && {
+  yarn install
+  yarn build
+}
+
+: "Deploy task" && {
+  yarn publish --non-interactive --use-yarnrc .yarnrc
+}
+
+: "After script of Deploy" && {
+  npx ts-node ./scripts/notify.ts
+}
