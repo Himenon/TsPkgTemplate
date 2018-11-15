3af2a1e8435814476cfa7713f202430ab783be95
Himenon <k.himeno314@gmail.com>
2018-11-15 14:13:59 +0900

    ci(deploy): 設定の更新

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,14 @@
 : "Before script of Deploy" && {
   yarn install
   yarn build
+  echo '//registry.npmjs.org/:_authToken="${NPM_TOKEN}"' >> .npmrc
 }
 
 : "Deploy task" && {
-  yarn publish --non-interactive --use-yarnrc .yarnrc
+  npm publish
 }
 
 : "After script of Deploy" && {
+  cp .npmrc.template .npmrc
   npx ts-node ./scripts/notify.ts
 }
