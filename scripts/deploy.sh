70341fe2ed21b402b05fd13a87be7976d0e22ce8
Himenon <6715229+Himenon@users.noreply.github.com>
2018-11-17 01:43:12 +0900

    ci(script): npmのバージョンアップのタイミングを修正

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,32 @@
 #! /bin/bash
 
+: "Prepare SSH Key for Github" && {
+  openssl aes-256-cbc -K $encrypted_xxxx_key -iv $encrypted_xxxx_iv -in travis_key.enc -out ~/.ssh/id_rsa -d
+  chmod 600 ~/.ssh/id_rsa
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+}
+
+: "Git Settings" && {
+  git config --global user.email "6715229+Himenon@users.noreply.github.com"
+  git config --global user.name "Himenon"
+}
+
 : "Before script of Deploy" && {
   yarn install
   yarn build
   echo '//registry.npmjs.org/:_authToken="${NPM_TOKEN}"' >> .npmrc
+}
+
+: "Generate Change Log" && {
+  npx changelog -p
+  git add CHANGELOG.md
+  git commit -m "chore(change-log): updated CHANGELOG.md"
+}
+
+# npx ts-node ./scripts/notify.ts
+# TODO: 自動で major, minor, patchを作る
+: "npm package version Up" && {
+  npm version patch
 }
 
 : "Deploy task" && {
