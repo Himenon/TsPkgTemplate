938ceba0d79fe7a670c893a59fd9c9289877f3f2
Himenon <6715229+Himenon@users.noreply.github.com>
2018-11-17 02:29:05 +0900

    ci(script): tsがない、対話モードの修正

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 : "Prepare SSH Key for Github" && {
   openssl aes-256-cbc -K $encrypted_3052b21f39dc_key -iv $encrypted_3052b21f39dc_iv -in travis_key.enc -out ~/.ssh/id_rsa -d
   chmod 600 ~/.ssh/id_rsa
-  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+  echo -e "Host github.com\n\tIdentityFile  ~/.ssh/id_rsa\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 }
 
 : "Git Settings" && {
